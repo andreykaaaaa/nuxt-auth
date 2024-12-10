1fe436761ccd63aa2684cb30e1a828453d41161b
Андрей Рыжинский <86745524+andreykaaaaa@users.noreply.github.com>
2024-12-10 15:54:14 +0300

    feat: инициализация проекта

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  ssr: true,
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script scoped lang="ts">
+</script>
+<template>
+  <div>
+    <h1>Auth</h1>
+  </div>
+</template>
